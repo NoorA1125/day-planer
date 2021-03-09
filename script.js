3c49680fe746780d775836a2d1ec71a6c13e81e9
Noor Abdikadir <nooromar305@gmail.com>
2021-03-09 03:40:03 -0700

    Connected the button to the input to save inputed information to user Data. Need to translate into JQuery, but until then, I will right it out in Vanilla Js.

--- script.js
+++ script.js
@@ -1,81 +1,16 @@
 var today = document.querySelector("#currentDay");
+var textArea = document.querySelector(".description");
 var container = document.querySelector(".container");
-var currentHour = moment().format("H");
-var $timeSlots = document.querySelector("#time-slots");
-//var startButton = document.querySelector("#startButton");
+hour = 9;
+var timeBlock;
+var saveButton = document.querySelector(".btn");
 
 
-today.innerHTML = moment().format("dddd MMMM Do YYYY h:mm:ss a");
+today.textContent = moment().format("dddd MMMM Do YYYY h:mm a");
+saveButton.addEventListener("click", saveTask);
 
-//***** Code for dynamically doing it */
-for (let i = 8; i <= 17; i++) {
-    $timeSlots.append(buildTimeSlot(i));
-    $timeSlots.innerHTML = currentHour;
-    
-}
-function buildTimeSlot(hour) {
-    // if statements showing past, present and future in differnet colors
-    var background = "background-color";
-    if (currentHour === hour) {
-      background = "present";
-    } else if (currentHour > hour) {
-      background = "past";
-    } else {
-      background = "future";
-    }
-    // actual js code building html
-    var $timeSlots = $("<div>")
-      .addClass("row time-block")
-      .attr("id", `hour-${hour}`);
-    var $hour = $("<div>").addClass("col-md-1 hour").text(hour);
-    var $textArea = $("<textarea>").addClass(
-      "col-md-10 description " + background
-    );
-    var $button = $("<button>")
-      .addClass("btn saveBtn col-md-1")
-      //fas and fas-save is the save btn icon
-      .append($("<i>").addClass("fas fa-save"));
-    //appending the code to the page
-    return $timeSlots.append($hour, $textArea, $button);
-  }
-
-
-
-
-// function createCalender() {
-//     container = document.querySelector(".container");
-//     var timeBlock = document.createElement("div");
-//     timeBlock.id = "hour-1";
-//     timeBlock.class = "row time-block";
-//     var hour = document.createElement("div");
-//     hour.class = "col-md-1 hour";
-//     timeBlock.appendChild(hour);
-//     //hour.innerHTML = moment().format(hh); (from 12AM - 12PM only)
-//     container.appendChild(timeBlock);
-//     draw();
-// }
-
-// function draw() {
-//     // (C1) QUESTION
-//     timeBlock.innerHTML = "";
-
-//     // (C2) OPTIONS
-//     container.innerHTML = "";
-//     container = document.querySelector("#container");
-//     for (let i in hour) {
-//         let radio = document.createElement("input");
-//         radio.type = "radio";
-//         radio.name = "quiz";
-//         radio.id = "quizo" + i;
-//         container.appendChild(radio);
-//         let label = document.createElement("label");
-//         label.innerHTML = quiz.data[now].o[i];
-//         label.setAttribute("for", "quizo" + i);
-//         label.dataset.idx = i;
-//         label.addEventListener("click", select);
-//         container.appendChild(label);
-//     }
-// };
-
-// createCalender();
-window.addEventListener("load", buildTimeSlot());
+//When the button is pressed - save to local storage
+textArea.value = localStorage.getItem('Things to do');
+function saveTask() {
+    localStorage.setItem('Things to do',textArea.value);
+};
